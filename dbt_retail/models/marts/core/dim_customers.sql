299b934c6daf7264dab96f84a513b2acfb795d12
R-midolli <rafaelmidolli@gmail.com>
2026-02-10 22:21:06 +0100

    fix: fix dim_customers to use ref() correct lineage

--- dbt_retail/models/marts/core/dim_customers.sql
+++ dbt_retail/models/marts/core/dim_customers.sql
@@ -1,8 +1,10 @@
+{{ config(materialized='table', schema='marts') }}
+
 with base as (
     select
         customer_id,
         max(country) as country
-    from staging.stg_sales
+    from {{ ref('stg_sales') }}
     where customer_id is not null
     group by 1
 )
